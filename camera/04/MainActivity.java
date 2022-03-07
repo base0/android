3f76951e4adcfc203a3117fde4aae1fb998a7e30
Base Zero <vacapp@live.com>
2022-03-07 10:39:54 +0700

    Create MainActivity.java

---
+++ camera/04/MainActivity.java
@@ -0,0 +1,88 @@
+public class MainActivity extends AppCompatActivity {
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA};
+    final int REQUEST_CODE_PERMISSIONS = 0;
+
+    boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            final int result = ContextCompat.checkSelfPermission(this, permission);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+    }
+
+    ImageAnalysis imageAnalyzer;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }
+    }
+
+    private void startCamera() {
+        final ListenableFuture<ProcessCameraProvider> future = ProcessCameraProvider.getInstance(this);
+        future.addListener(() -> {
+            try {
+                //
+                imageAnalyzer = new ImageAnalysis.Builder().build();
+                imageAnalyzer.setAnalyzer(Executors.newSingleThreadExecutor(), new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(@NonNull ImageProxy image) {
+                        // Log.i("vac", "analyze: " + image.getFormat());  // YUV_420_888
+                        // Y     luminance
+                        // U,V   chrominance (blue and red)
+                        /* https://developer.android.com/reference/android/graphics/ImageFormat#YUV_420_888
+                          The order of planes in the array returned by Image#getPlanes() is guaranteed such that plane #0 is always Y, plane #1 is always U (Cb), and plane #2 is always V (Cr). */
+                        //Log.i("vac", "analyze: " + image.getWidth() + " " + image.getHeight());
+                        //Log.i("vac", "analyze: " + image.getPlanes()[0].getBuffer().remaining());
+                        ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+                        byte[] a = new byte[byteBuffer.remaining()];
+                        byteBuffer.get(a);
+                        int sum = 0;
+                        for (byte b : a) {
+                            sum += b & 0xFF;
+                        }
+                        double d = sum;
+                        d /= a.length;
+                        Button button = findViewById(R.id.camera_capture_button);
+                        button.setText(String.format("%.2f", d));
+                        // byte[] a = byteBuffer.array();  // UnsupportedOperationException
+                        image.close();
+                    }
+                });
+
+                ProcessCameraProvider cameraProvider = future.get();
+                Preview preview = new Preview.Builder().build();
+                PreviewView viewFinder = findViewById(R.id.viewFinder);
+                preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
+                cameraProvider.unbindAll();
+                //cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview);
+                //cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture, imageAnalyzer);
+                cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, preview, imageAnalyzer);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+}
